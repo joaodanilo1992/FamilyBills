2b344e23fdbd8fc56f55c3231f1dbfe0feaff4a2
João Danilo <joaodanilo1992@gmail.com>
2019-03-03 12:35:48 -0400

    Added splash activity

---
+++ app/src/main/java/com/example/familybills/Splash.java
@@ -0,0 +1,27 @@
+package com.example.familybills;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
